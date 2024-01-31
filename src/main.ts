e4247fc231fabfe12da24b458a268be794c77fa6
sofhie <156117670+sooofhy@users.noreply.github.com>
2024-01-31 17:43:31 -0300

    criação de rota

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { db, firestore } from "../banco_de_dados/firebase";
 
 const app = express();
 
+app.get('/',(req, res) => {
+    res.send ('Bem vindo a API')
+})
 
+app.post('/usuario', async(req, res) =>{
+    const usuario = req.body.nome 
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'),{
+            nome: nome, 
+        })
+
+        res.send(docRef.id)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
 
 app.listen(3000,function (){
-    console.log('Serviço Rodando em https://localhost:3000')
+    console.log('Serviço Rodando em http://localhost:3000')
 })
